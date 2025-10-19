1460c77a75a31c09833ef8b5d415e1a914d5c553
Sara <153566273+SaraYehia66@users.noreply.github.com>
2025-10-19 17:21:18 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,3 +8,11 @@ window.onscroll= function(){
         header.classList.remove("active")
     }
 }
+
+let nav_links = document.getElementById("links");
+
+function open_close_menu(){
+    nav_links.classList.toggle("active")
+
+}
+
